a12344f19a6eb1914a9659c9b7f5493b3d3d3a4e
Shubham Mishra <shubhamm10.06.2006@gmail.com>
2025-08-20 22:44:30 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import ThemeToggler from "@/components/ui/theme-toggle-button";
+import Header from "./../components/header"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
